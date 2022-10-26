120c8ce60a1ae66a4a3a77faa87188549e2fa02b
Alexander <alex@arekusandanoMacBook-Air.local>
2022-10-26 19:11:06 +0900

    java_4

---
+++ practice/Chapter04.java
@@ -0,0 +1,9 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        String rei = "watashi";
+        // System.out.println(rei);
+        rei = "ore";
+        final int HOME_SIZE = 7;
+        System.out.println(HOME_SIZE);
+    }
+}
